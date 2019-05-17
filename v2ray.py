fb10ed82df8761369bb256e5e00f56e532a2b57b
xbblog <f550529870@gmail.com>
2019-05-17 18:37:50 +0800

    使用标准配置

--- v2ray.py
+++ v2ray.py
@@ -24,18 +24,60 @@ class V2ray(Node):
         self.camouflageType = camouflageType
 
     def formatConfig(self):
+        v2rayConf = {
+          "log" : {
+              "access" : "/var/log/v2ray/access.log",
+              "error":"/var/log/v2ray/error.log",
+              "logLevel": "none"
+          },
+          "inbounds": [
+            {
+              "port": 1080,
+              "listen": "127.0.0.1",
+              "protocol": "socks",
+              "settings": {
+                "udp": True
+              },
+              "tag": "in"
+            }
+          ],
+          "outbounds": [
+            {
+              "settings" : {},
+              "protocol":"freedom",
+              "tag" : "direct"
+            },
+            {
+                "settings" : {},
+                "protocol":"blackhole",
+                "tag" : "blocked"
+            }
+          ],
+          "routing": {
+            "strategy": "rules",
+            "settings": {
+              "domainStrategy": "AsIs",
+              "rules": [
+                {
+                  "type" : "field",
+                  "ip" : [
+                    "geoip:cn",
+                    "geoip:private"
+                  ],
+                  "outboundTag": "direct"
+                },
+                {
+                  "type":"field",
+                  "inboundTag":["in"],
+                  "outboundTag":"out"
+                }
+              ]
+            }
+          }
+        }
         if self.network == 'tcp' or self.network == 'auto':
             # tcp下
-            v2rayConf = {
-                "inbound": {
-                    "port": 1080,
-                    "listen": "127.0.0.1",
-                    "protocol": "socks",
-                    "settings": {
-                        "udp": True
-                    }
-                },
-                "outbound": {
+            v2rayConf['outbounds'].append({
                     "protocol": "vmess",
                     "settings": {
                         "vnext": [{
@@ -48,54 +90,13 @@ class V2ray(Node):
                                 }
                             ]
                         }]
-                    }
-                },
-                "outboundDetour": [{
-                    "protocol": "freedom",
-                    "tag": "direct",
-                    "settings": {}
-                }],
-                "routing": {
-                    "strategy": "rules",
-                    "settings": {
-                        "domainStrategy": "AsIs",
-                        "rules": [{
-                            "type": "field",
-                            "ip": [
-                                "0.0.0.0/8",
-                                "10.0.0.0/8",
-                                "100.64.0.0/10",
-                                "127.0.0.0/8",
-                                "169.254.0.0/16",
-                                "172.16.0.0/12",
-                                "192.0.0.0/24",
-                                "192.0.2.0/24",
-                                "192.168.0.0/16",
-                                "198.18.0.0/15",
-                                "198.51.100.0/24",
-                                "203.0.113.0/24",
-                                "::1/128",
-                                "fc00::/7",
-                                "fe80::/10"
-                            ],
-                            "outboundTag": "direct"
-                        }]
-                    }
-                }
-            }
+                    },
+                    "tag": "out"
+                })
             return v2rayConf
         elif self.network == 'kcp':
             # kcp 下
-            v2rayConf = {
-                "inbound": {
-                    "port": 1080,
-                    "listen": "127.0.0.1",
-                    "protocol": "socks",
-                    "settings": {
-                        "udp": True
-                    }
-                },
-                "outbound": {
+            v2rayConf['outbounds'].append({
                     "protocol": "vmess",
                     "settings": {
                         "vnext": [{
@@ -123,54 +124,13 @@ class V2ray(Node):
                                 "type": self.camouflageType,
                             }
                         }
-                    }
-                },
-                "outboundDetour": [{
-                    "protocol": "freedom",
-                    "tag": "direct",
-                    "settings": {}
-                }],
-                "routing": {
-                    "strategy": "rules",
-                    "settings": {
-                        "domainStrategy": "AsIs",
-                        "rules": [{
-                            "type": "field",
-                            "ip": [
-                                "0.0.0.0/8",
-                                "10.0.0.0/8",
-                                "100.64.0.0/10",
-                                "127.0.0.0/8",
-                                "169.254.0.0/16",
-                                "172.16.0.0/12",
-                                "192.0.0.0/24",
-                                "192.0.2.0/24",
-                                "192.168.0.0/16",
-                                "198.18.0.0/15",
-                                "198.51.100.0/24",
-                                "203.0.113.0/24",
-                                "::1/128",
-                                "fc00::/7",
-                                "fe80::/10"
-                            ],
-                            "outboundTag": "direct"
-                        }]
-                    }
-                }
-            }
+                    },
+                    "tag": "out"
+                })
             return v2rayConf
         elif self.network == 'ws':
             # ws
-            v2rayConf = {
-                "inbound": {
-                    "port": 1080,
-                    "listen": "127.0.0.1",
-                    "protocol": "socks",
-                    "settings": {
-                        "udp": True
-                    }
-                },
-                "outbound": {
+            v2rayConf['outbounds'].append({
                     "protocol": "vmess",
                     "settings": {
                         "vnext": [{
@@ -196,54 +156,13 @@ class V2ray(Node):
                                 "Host": self.camouflageHost
                             }
                         }
-                    }
-                },
-                "outboundDetour": [{
-                    "protocol": "freedom",
-                    "tag": "direct",
-                    "settings": {}
-                }],
-                "routing": {
-                    "strategy": "rules",
-                    "settings": {
-                        "domainStrategy": "AsIs",
-                        "rules": [{
-                            "type": "field",
-                            "ip": [
-                                "0.0.0.0/8",
-                                "10.0.0.0/8",
-                                "100.64.0.0/10",
-                                "127.0.0.0/8",
-                                "169.254.0.0/16",
-                                "172.16.0.0/12",
-                                "192.0.0.0/24",
-                                "192.0.2.0/24",
-                                "192.168.0.0/16",
-                                "198.18.0.0/15",
-                                "198.51.100.0/24",
-                                "203.0.113.0/24",
-                                "::1/128",
-                                "fc00::/7",
-                                "fe80::/10"
-                            ],
-                            "outboundTag": "direct"
-                        }]
-                    }
-                }
-            }
+                    },
+                    "tag": "out"
+                })
             return v2rayConf
         else:
             # h2
-            v2rayConf = {
-                "inbound": {
-                    "port": 1080,
-                    "listen": "127.0.0.1",
-                    "protocol": "socks",
-                    "settings": {
-                        "udp": True
-                    }
-                },
-                "outbound": {
+            v2rayConf['outbounds'].append({
                     "protocol": "vmess",
                     "settings": {
                         "vnext": [{
@@ -269,40 +188,8 @@ class V2ray(Node):
                                 self.camouflageHost
                             ]
                         }
-                    }
-                },
-                "outboundDetour": [{
-                    "protocol": "freedom",
-                    "tag": "direct",
-                    "settings": {}
-                }],
-                "routing": {
-                    "strategy": "rules",
-                    "settings": {
-                        "domainStrategy": "AsIs",
-                        "rules": [{
-                            "type": "field",
-                            "ip": [
-                                "0.0.0.0/8",
-                                "10.0.0.0/8",
-                                "100.64.0.0/10",
-                                "127.0.0.0/8",
-                                "169.254.0.0/16",
-                                "172.16.0.0/12",
-                                "192.0.0.0/24",
-                                "192.0.2.0/24",
-                                "192.168.0.0/16",
-                                "198.18.0.0/15",
-                                "198.51.100.0/24",
-                                "203.0.113.0/24",
-                                "::1/128",
-                                "fc00::/7",
-                                "fe80::/10"
-                            ],
-                            "outboundTag": "direct"
-                        }]
-                    }
-                }
-            }
+                    },
+                    "tag": "out"
+                })
             return v2rayConf
-
+1
